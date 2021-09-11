5a6318d979078f059fe93661b40fd53caffa2b9d
melike2d <gino@femboy.email>
2021-09-11 00:22:27 -0700

    Remove opus dependency and update everything else

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     id("com.github.johnrengelman.shadow") version "5.1.0"
 }
 
-val versionObj = Version(major = "4", minor = "3", revision = "0")
+val versionObj = Version(major = "4", minor = "4", revision = "0")
 
 project.group = "net.dv8tion"
 project.version = "$versionObj"
@@ -62,33 +62,23 @@ dependencies {
 
     //Code safety
     api("com.google.code.findbugs:jsr305:3.0.2")
-    api("org.jetbrains:annotations:16.0.1")
+    api("org.jetbrains:annotations:22.0.0")
 
     //Logger
-    api("org.slf4j:slf4j-api:1.7.25")
+    api("org.slf4j:slf4j-api:1.7.32")
 
     //Web Connection Support
     api("com.neovisionaries:nv-websocket-client:2.14")
-    api("com.squareup.okhttp3:okhttp:3.13.0")
-
-    //Opus library support
-    api("club.minnced:opus-java:1.1.0@pom") {
-        isTransitive = true
-    }
+    api("com.squareup.okhttp3:okhttp:4.9.1")
 
     //Collections Utility
-    api("org.apache.commons:commons-collections4:4.1")
-
-    //we use this only together with opus-java
-    // if that dependency is excluded it also doesn't need jna anymore
-    // since jna is a transitive runtime dependency of opus-java we don't include it explicitly as dependency
-    compileOnly("net.java.dev.jna:jna:4.4.0")
+    api("org.apache.commons:commons-collections4:4.4")
 
     /* Internal dependencies */
 
     //General Utility
     implementation("net.sf.trove4j:trove4j:3.0.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.5")
 
     //Sets the dependencies for the examples
     configurations["examplesImplementation"].withDependencies {
@@ -96,7 +86,7 @@ dependencies {
         addAll(configurations["implementation"].allDependencies)
     }
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.4.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
 }
 
 val compileJava: JavaCompile by tasks
